e0b8513ee28dc03c6fb10926c9958fa72b37df66
Jake Goodwin <jakegoodwin1337@gmail.com>
2017-02-07 18:43:58 -0800

    Update Main.py
    Now functioning with user interface and outputting at 1v increments up to 5v.

--- Main.py
+++ Main.py
@@ -4,17 +4,30 @@
 #importing libary for gui
 import tkinter as tk
 
+#global vars
+output_list = []
+
 #functions 
 
-#Takes the values out of the text boxes
-def grab_values(event):
-    print("values taken!")
-    test = tk.Label(root, text = "Calculated values:")
-    test.grid(row = 3, column = 1, sticky = tk.W)
-
+#Takes the values out of the text boxes and calls formula function.
 def retrieve_input(event):
-    input_vp = vp_input.get()
-    print(input_vp)
+    input_vp = float(vp_input.get())
+    input_idss = float(Idss_input.get())
+    apply_trans_formula(input_vp, input_idss)
+
+#uses the values and runs them through the formula.
+def apply_trans_formula(vp, idss):
+    idss = idss/1000
+    for index in range(0, 6, 1):
+        num = format((1000*(idss * ((1-(index / vp))** 2))), '10.6f')
+        line = str('Amperage at: '+ str(index) + ' v : '+ str(num) + 'mA')
+        x = index + 2
+        new = tk.Label(root, text = line)
+        new.grid(row = x, column = 0, sticky = tk.W)
+        #output_list.append(line)
+    #output_data = tk.Label(root, str(output_list))
+    #output_data.grid(row = 2, column = 0, sticky = tk.W)
+        
 #root for gui window
 root = tk.Tk()
 
@@ -31,6 +44,7 @@ current_drain.grid(row = 1, column = 0, sticky = tk.E)
 #add input boxes
 vp_input = tk.Entry(root)
 vp_input.grid(row = 0, column = 1)
+#vp_input.delete(0, tk.END)
 
 Idss_input = tk.Entry(root)
 Idss_input.grid(row = 1, column = 1)
